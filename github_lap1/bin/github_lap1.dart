534d521e7266380dbbfcfa3ce98d7f3801c8f607
Rahafahg <irahaf.88@gmail.com>
2024-07-31 11:42:26 +0300

    branch puch test

--- github_lap1/bin/github_lap1.dart
+++ github_lap1/bin/github_lap1.dart
@@ -1,13 +1,12 @@
 void main(List<String> arguments) {
   List a = [1, 2, 3, 4, 5, 6];
-
-  print(addeven(a: a));
+  print(a);
+  print(add(a: a));
 }
 
-int addeven({required List a}) {
+int add({required List a}) {
   int count = 0;
   for (int element in a) {
-    if (element % 2 == 0) ;
     count += element;
   }
   return count;
