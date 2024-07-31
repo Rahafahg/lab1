67a9ffbe621c19a2a3ae1735e129d00468341e70
Rahafahg <irahaf.88@gmail.com>
2024-07-31 11:23:25 +0300

    function added

--- github_lap1/bin/github_lap1.dart
+++ github_lap1/bin/github_lap1.dart
@@ -1,5 +1,14 @@
-import 'package:github_lap1/github_lap1.dart' as github_lap1;
-
 void main(List<String> arguments) {
-  print('Hello world: ${github_lap1.calculate()}!');
+  List a = [1, 2, 3, 4, 5, 6];
+
+  print(addeven(a: a));
+}
+
+int addeven({required List a}) {
+  int count = 0;
+  for (int element in a) {
+    if (element % 2 == 0) ;
+    count += element;
+  }
+  return count;
 }
